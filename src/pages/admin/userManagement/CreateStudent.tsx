24f1e3f7d74a2b624d0159b49cdd6e6f1ea95de7
aothymoon59 <aothymoon59@gmail.com>
2024-05-20 22:36:01 +0600

    [CREATED] student basic form structure

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,7 @@
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 
 const CreateStudent = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
@@ -11,13 +11,38 @@ const CreateStudent = () => {
     formData.append("something", "Data of something");
 
     //! This is for development
+    //! Just for checking
     console.log(Object.fromEntries(formData));
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider>Personal Info.</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="dateOfBirth" label="Date Of Birth" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="bloodGroup" label="Blood Group" />
+            </Col>
+          </Row>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
